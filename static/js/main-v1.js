77200b8d7e422c62a2736b15bb390f5711fa7d4a
cp2004 <31997505+cp2004@users.noreply.github.com>
2020-12-04 18:12:49 +0000

    Add version history chart

--- static/js/main-v1.js
+++ static/js/main-v1.js
@@ -18,14 +18,24 @@ function getData() {
         console.log(data)
         for (let plugin in data){var container = document.getElementById('statsOverview').children[0];
             if (container){
-                var graph = document.createElement("div")
-                graph.id = plugin
-                var wrapper = document.createElement("div")
-                wrapper.className = "col-md-6"
-                wrapper.appendChild(graph)
-                container.appendChild(wrapper)
+                var pluginContainer = document.createElement("div")
+                pluginContainer.id = plugin + "Container"
+                pluginContainer.className = "col-md-12 row"
+
+                var versionGraph = document.createElement("div")
+                versionGraph.id = plugin + "Version"
+                versionGraph.className = "col-md-6"
+                pluginContainer.appendChild(versionGraph)
+
+                var historyGraph = document.createElement("div")
+                historyGraph.id = plugin + "History"
+                historyGraph.className = "col-md-6"
+                pluginContainer.appendChild(historyGraph)
+
+                container.appendChild(pluginContainer)
             }
-            createVersionsChart(data[plugin], plugin, names[plugin]);
+            createVersionsChart(data[plugin], plugin + "Version", names[plugin] + "Versions");
+            createHistoryChart(data[plugin], plugin + "History", names[plugin] + "History (30 days)")
         }
     })
 }
@@ -61,6 +71,51 @@ function createVersionsChart(data, element, name) {
     } catch (error) {
         console.log(name, error)
     }
+}
+
+function createHistoryChart(data, element, name){
+    try{
+        var x_vals = []
+        var versions = []
+        for (let date in data.history){
+            x_vals.push(data.history[date].date)
+            for (let version in data.history[date].versions){
+                if (!versions.includes(version)){
+                    versions.push(version)
+                }
+            }
+        }
+        console.log(x_vals)
+        var lines = []
+
+        for (let versionIndex in versions) {
+            var version = versions[versionIndex]
+            var y_vals = []
+            for (let date in data.history) {
+                try {
+                    y_vals.push(data.history[date].versions[version].instances)
+                } catch (e) {
+                    // if the version didn't exist on that date, line should be at 0
+                    y_vals.push(0)
+                }
+            }
+            console.log(y_vals)
+            lines.push({
+                x: x_vals,
+                y: y_vals,
+                mode: 'lines',
+                name: version
+            })
+        }
+        layout = {
+            title: name,
+        }
+        console.log(lines)
+        Plotly.newPlot(element, lines, layout)
+    } catch (e) {
+        console.log(name, e)
+    }
+
 }
 
 function loadPage() {
