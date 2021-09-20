3f41cd3efe2b4708c0ca22bac04f50b105125e08
cp2004 <31997505+cp2004@users.noreply.github.com>
2021-09-21 00:02:38 +0100

    :sparkles: Add footer

--- components/Layout.js
+++ components/Layout.js
@@ -1,6 +1,8 @@
 import AppBar from "./AppBar"
 import Container from "@mui/material/Container";
-import {styled} from "@mui/material";
+import {Divider, Stack, styled} from "@mui/material";
+import Typography from "@mui/material/Typography";
+import Link from "./Link";
 
 const Offset = styled('div')(({theme}) => theme.mixins.toolbar)
 
@@ -12,7 +14,15 @@ export default function Layout (props) {
             <Container maxWidth={false} sx={{marginTop: {xs: 4, sm: 2}}}>
                 {props.children}
             </Container>
-            {/* TODO add a footer */}
+            <Divider />
+            <Stack sx={{my: 8, alignItems: 'center', "& > .MuiTypography-root": {my: 2}}}>
+                <Typography variant={"h6"}>
+                    OctoPluginStats | Created by Charlie Powell
+                </Typography>
+                <Typography variant={"h6"}>
+                    Stats provided by OctoPrint's <Link href={"https://tracking.octoprint.org"} target={"_blank"}>anonymous usage tracking</Link> exports.
+                </Typography>
+            </Stack>
         </>
     )
 }
